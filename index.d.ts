ff03adeca831eafdc119e06d330c83748becb2c7
Lee Campbell <lee.campbell@evergreen-life.co.uk>
2020-03-29 20:06:54 +0100

    v0.0.17

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,13 @@
-export declare function convert(inputGlob: string, outputDirectory?: string, options?: any): Promise<void>;
+declare class ConversionOptions {
+    cwd?: string;
+    verbose: boolean;
+    breakOnSchemaValidationErrors: boolean;
+}
+export declare function convert(inputGlob: string, outputDirectory?: string, options?: ConversionOptions): Promise<void>;
+declare class DeploymentOptions {
+    connectionString?: string;
+    fileNamesAsCollectionNames?: boolean;
+}
+export declare function deploy(bsonSchemaGlob: string, deploymentOptions: DeploymentOptions): Promise<void>;
+export declare function addIndices(bsonFileGlob: string): Promise<void>;
+export {};
